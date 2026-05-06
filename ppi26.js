ab123268cccfae21dfb7c8cf628e59d9a677cc15
joaovitorleonardo21 <joao.leonardo@escolar.ifrn.edu.br>
2026-05-06 19:19:35 +0000

    aula dia 6/05 - introdução ao javaScript

---
+++ ppi26.js
@@ -0,0 +1,35 @@
+// prpogramação para internet
+// aluno: João vitor leonardo
+
+//introdução ao javaScript
+
+// ATIVAR O FORMATADOR PRETTIER
+// ALT + SHIFT + F
+
+
+console.log("bem vindos ao ppi");
+
+// variaveis
+let name = "joão vitor";
+let age = 17;
+let isStduant = true;
+
+const PI = 3.14; // não pode mudar de valor
+console.log(PI);
+
+// operadores aritiméticos
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "hello" + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+// Operadorse relacionais
+console.log(x '5' == 5); // Compara tipo OU valor 
+console.log(x '5' === 5); // Compara tipo E valor 
+
+// Operadores de incremento
+console.log(x++);
+console.log(x);
+
